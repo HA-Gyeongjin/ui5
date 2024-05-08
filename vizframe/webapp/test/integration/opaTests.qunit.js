70a09a0feb3a5e1c8da3a686469a7ece9bc8c964
HA-Gyeongjin <wid5785@gmail.com>
2024-05-08 18:27:10 +0900

    20240508

---
+++ vizframe/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sync/ec/vizframe/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ vizframe/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"syncec/vizframe/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
